9fedce50e91e3bc222550abe8c6bff99b34aa5f9
Jurfest <jurfest.apps@gmail.com>
2022-12-13 11:26:55 -0300

    feat: setup prisma and list and post methods for notifications

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaPromise } from '@prisma/client';
+import { randomUUID } from 'node:crypto';
 
-@Controller()
+import { PrismaService } from './prisma/prisma.service';
+
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list(): PrismaPromise<any> {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: any): Promise<void> {
+    const { recipientId, content, category } = body;
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
